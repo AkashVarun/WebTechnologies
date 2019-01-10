9c324832108e6048fb108c0441f1d9358ec5502d
AkashVarun <akash.varun96@gmail.com>
2019-01-10 22:19:55 +0530

    Javascript

---
+++ test.js
@@ -0,0 +1,35 @@
+/*
+
+var str ="hello angular js ";
+//console.log(str);
+document.writeln(str);
+document.writeln(typeof str);
+document.writeln(str + " <br>");
+  
+//string
+var num = 10;
+document.writeln(num);
+document.writeln(typeof num);
+//number data type
+
+var boolean = true;
+document.writeln(typeof boolean);
+//boolean data type
+
+var undefined;
+document.writeln(typeof undefined);
+//undefined data type
+
+var null1 = null;
+document.writeln(typeof null1);
+//null datatype
+
+
+*/
+
+var array = ["java" , "python" , "nodejs" , 10 , true , undefined , null , {} ]
+console.log(array);
+
+
+
+
